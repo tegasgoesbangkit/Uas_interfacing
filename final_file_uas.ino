5ceb526c66109656ca84b67502e8abe74c60374e
tegasgoesbangkit <77987763+tegasgoesbangkit@users.noreply.github.com>
2021-01-26 02:14:04 +0800

    Add files via upload

---
+++ final_file_uas.ino
@@ -0,0 +1,20 @@
+
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+
+char auth[] = "Ej1gp7k-2yFTUqzW8HXP8riR9AD7UsNo";
+
+char ssid[] = "Galaxy S10+be44";
+char pass[] = "beliquota123";
+
+void setup(){
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop(){
+  Blynk.run();
+}
